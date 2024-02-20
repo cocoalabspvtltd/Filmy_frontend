c187f7d55c0b94bc3a693bd88697f9ae3b09dee7
Nimacocolabs <nimacocoalabs@gmail.com>
2024-02-20 15:06:10 +0530

    First commit

--- lib/screens/professional/p_home_screen.dart
+++ lib/screens/professional/p_home_screen.dart
@@ -15,21 +15,57 @@ class _PHomeScreenState extends State<PHomeScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black54,
-                    blurRadius: 4.0,
-                    spreadRadius: 0.0,
-                  ), //BoxShadow
-                ],
+            // Padding(
+            //   padding: const EdgeInsets.only(
+            //       top: 15.0, left: 10, right: 10, bottom: 4),
+            //   child: Column(
+            //     mainAxisAlignment: MainAxisAlignment.start,
+            //     crossAxisAlignment: CrossAxisAlignment.start,
+            //     children: [
+            //       const Text(
+            //         "Friend's you might know",
+            //         style: TextStyle(
+            //             fontWeight: FontWeight.bold, fontSize: 16),
+            //       ),
+            //       const SizedBox(
+            //         height: 8,
+            //       ),
+            //       // _youKnowWidget(context),
+            //     ],
+            //   ),
+            // )
+            Card(
+              elevation: 4.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
               ),
-              child: Column(
-                children: [
-                  Text("Create Project")
-                ],
+              child: InkWell(
+                onTap: () {
+                  // Add functionality to create project
+                },
+                borderRadius: BorderRadius.circular(8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        Icons.add,
+                        size: 36.0,
+                        color: Colors.blue,
+                      ),
+                      SizedBox(height: 16.0),
+                      Text(
+                        'Create Project',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
